18450fcc8b0904d9a80875337cb7c4ce62f8a0b4
Etienne BLANC-COQUAND <etienne1296pro@gmail.com>
2018-05-31 12:24:21 +0200

    Add todos and colors

--- script.sh
+++ script.sh
@@ -1,1 +1,20 @@
 #! /bin/bash
+
+## COLORS
+red=`tput setaf 1`;
+green=`tput setaf 2`;
+yellow=`tput setaf 3`;
+blue=`tput setaf 4`;
+magenta=`tput setaf 5`;
+orange=`tput setaf 33`;
+noColor=`tput sgr0`;
+
+# TODO : Faire le menu principal
+
+# TODO : Installer Vagrant et/ou VirtualBox
+
+# TODO : Création d'une Vagrant comprenant le fichier VagrantFile
+
+# TODO : Afficher les Vagrant en fonctionnement sur le système et interragir avec
+
+# TODO : Placer les erreurs dans un fichier error.log
